24e8fe4d7d097014be1fac75b3d4cb6f0249c439
JitenKumar <jitenderpalsra@gmail.com>
2018-05-26 11:44:20 +0530

    Universal Array Functions

---
+++ Numpy Practice/Universal Array Functions/universal.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+# arr1 = np.arange(11)
+# print(arr1.sqrt(arr1))
+# print(arr1)
+# random numbers
+ArrA = np.random.randn(10)
+print(ArrA)
+ArrB = np.random.randn(10)
+# Binary Functions0
+print(np.add(ArrA,ArrB))
+# maximum
+print(np.maximum(ArrA,ArrB))
+
+
+
